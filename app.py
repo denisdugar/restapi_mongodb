b927a503352e49922d7f2d81cfdb9ef9985d5e9f
denisdugar <denisdugar@gmail.com>
2021-10-13 19:33:55 +0300

    update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 from numpy import string_
 
 app = Flask(__name__)
-app.config["MONGO_URI"] ="mongodb+srv://denis:254693178521@cluster0.k6yf1.mongodb.net/Hotel?retryWrites=true&w=majority"
+app.config["MONGO_URI"] ="mongodb+srv://<login>:<password>@cluster0.k6yf1.mongodb.net/<database>?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
 ########## COUNT OF OBJECTS ############
